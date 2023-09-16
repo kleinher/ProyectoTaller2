cba71fe40280a2db852560be808cb49cbd0dad7f
Victor Duarte <42872959+v-duarte@users.noreply.github.com>
2023-09-16 19:15:56 -0300

    Subida de codigo arduino
    Leer el ReadMe.
    El siguiente codigo deberia realizar las siguientes tareas:
    Conectarse al servidor EMQX (con reconexion en caso de fallo)
    Medir la temperatura
    Enviarla al servidor como JSON

---
+++ Arduino/Proyecto.ino
@@ -0,0 +1,124 @@
+//Establece el modelo del modulo GPRS a utilizar
+#define TINY_GSM_MODEM_SIM800
+// Incluimos librerías DHT11 
+#include <DHT.h>
+#include <DHT_U.h>
+//Para emular la comunicación serial en otros pines digitales
+#include <SoftwareSerial.h>
+//Para uso de JSON
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+//Para facilitar uso de GPRS
+#include <TinyGsmClient.h>
+#include <PubSubClient.h> 
+
+// Definimos el pin digital donde se conecta el sensor "DHT11"
+#define DHTPIN 5
+// Dependiendo del tipo de sensor
+#define DHTTYPE DHT11
+
+//Variables globales
+
+DHT dht(DHTPIN, DHTTYPE); // Inicializamos el sensor DHT11
+
+//Se crea objeto de comunicacion serial para comunicarse con SIM800L
+//SIM800L TX y RX estan conectados a los pines 3 y 2 de Arduino Uno
+SoftwareSerial serialSIM(3, 2);
+
+const char* apn = "TuAPN"; // Configura el APN de tu proveedor de servicios móviles
+const char* gsmPin = "TuPIN"; // Si tu tarjeta SIM tiene un PIN, configúralo aquí
+
+const char* mqttServer = "163.10.3.73";
+const int mqttPort = 8083;
+const char* mqttUser = "taller2g2";
+const char* mqttPassword = "taller2g2";
+
+TinyGsm modem(serialSIM);
+TinyGsmClient client(modem);
+
+PubSubClient mqtt(client);
+
+void setupDHT11(){
+  // Comenzamos el sensor DHT
+  dht.begin();
+}
+ 
+void setup() {
+ // Inicializamos comunicación serie
+  Serial.begin(9600);
+  
+  //Begin serial communication with Arduino and SIM800L
+  serialSIM.begin(9600);
+
+  //Inicializacion sensor de temperatura 
+  setupDHT11();
+
+  // Conecta al módulo SIM800L
+  if (!modem.init()) {
+    Serial.println("Error al iniciar el módulo GSM");
+    while (true);
+  }
+
+  // Desbloquea la tarjeta SIM con PIN si es necesario
+  // if (gsmPin && modem.simUnlock(gsmPin) != GSM_SUCCESS) {
+  //   Serial.println("Error al desbloquear la tarjeta SIM");
+  //   while (true);
+  // }
+
+  // Conéctate a la red GPRS utilizando el APN
+  if (!modem.gprsConnect(apn)) {
+    Serial.println("Error al conectarse a la red GPRS");
+    while (true);
+  }
+
+  // Configura el servidor MQTT
+  mqtt.setServer(mqttServer, mqttPort);
+}
+
+void loop() {
+  if (!mqtt.connected()) {
+    reconnect(); //Reconecto si se pierde la señal
+  }
+
+   //Leemos los datos del sensor DHT11.
+  float temp = dht.readTemperature(); // Leemos la temperatura en grados centigrados (por defecto)
+  // Comprobamos si ha habido algun error en la lectura
+  if (!(isnan(temp))) { //Si no hubo error al obtener los datos del sensor DHT11.
+    // Serial.print("Temperatura: ");
+    // Serial.print(temp);
+    // Serial.print(" *C ");
+    // Serial.print("\n");
+
+     // Crear un objeto JSON
+    DynamicJsonDocument jsonDoc(200);
+    jsonDoc["temperatura"] = temp;
+    char jsonBuffer[200];
+    serializeJson(jsonDoc, jsonBuffer);
+
+    // Publicar el JSON como mensaje MQTT
+    mqtt.publish("sensor/temperatura", jsonBuffer);
+  }
+  else{
+    // Si hay un error al leer la temperatura, enviar un mensaje de error en formato JSON.
+    DynamicJsonDocument errorDoc(200);
+    errorDoc["error"] = "Error al leer temperatura";
+    char errorBuffer[200];
+    serializeJson(errorDoc, errorBuffer);
+    mqtt.publish("sensor/error", errorBuffer);
+  }
+  delay(5000); // Esperamos 5 segundos entre medidas
+}
+
+void reconnect() { //Modulo para reconexion
+  while (!mqtt.connected()) {
+    Serial.println("Conectando al servidor MQTT...");
+    if (mqtt.connect("arduinoClient", mqttUser, mqttPassword)) {
+      Serial.println("Conectado al servidor MQTT");
+    } else {
+      Serial.print("Error de conexión MQTT, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" Intentando nuevamente en 5 segundos");
+      delay(5000);
+    }
+  }
+}
---
+++ Arduino/ReadMe.txt
@@ -0,0 +1,11 @@
+Para configurar el arduino debes realizar lo siguiente:
+
+- Configurar APN Linea 28
+- (Opcional) Configurar pin de la tarjeta linea 29
+
+- EMQX: Revisar los canales Linea 99 y linea 107. para que coincidan con los canales del servidor.
+
+despues ya esta todo configurado con el usuario, contraseña y puerto del Servidor.
+
+PD: Pin 5 es el Data del DHT11.
+Los pines del modulo GPRS son tal y como lo configuro Hernan (revisar, linea 26)
